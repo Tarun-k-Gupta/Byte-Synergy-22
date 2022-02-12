9615e601b18b8ce48b64d7e279f7be7557a4e838
N V Sai Likhith <nvsailikhith@gmail.com>
2022-02-12 22:07:38 +0530

    working fine

--- Math.py
+++ Math.py
@@ -9,70 +9,14 @@ def botSpeak(data):
     os.system("python3 playAudio.py")
 
 def errorcall():
+    print("Sorry! That doesn't bring up any result")
     botSpeak("Sorry! That doesn't bring up any result")
 
 def succcesscall(result, operation):
+    print("The result of the " + operation + " is " + result)
     botSpeak("The result of the " + operation + " is " + result)
 
 
-def getNumbers(text):
-    p = inflect.engine()
-    keyNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
-                'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 
-                'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 
-                'hundred', 'thousand', 'lakh', 'crore']
-
-
-    digits = keyNumbers[0:10]
-    multipliers = keyNumbers[28:]
-
-    words = text.split()
-    words.append('null')
-
-
-    grandFinalNumbers = []
-    numbers = []
-
-    for word in words:
-        if word in keyNumbers:
-            numbers.append(word)
-        elif (word == 'not'):
-            numbers.append('_')
-        elif ((word == 'and') and (numbers[len(numbers)-1] in multipliers)):
-            numbers.append("+")
-        else:
-            i = 0
-            finalNumbers = []
-            while (i < len(numbers)):
-                if numbers[i] in keyNumbers:
-                    if(len(finalNumbers) != 0):
-                        if numbers[i] in multipliers :
-                            finalNumbers[len(finalNumbers) - 1] *= w2n.word_to_num(numbers[i])
-                            i += 2
-                        else:
-                            finalNumbers[len(finalNumbers) - 1] += w2n.word_to_num(numbers[i])
-                            i += 2
-                    else:
-                        finalNumbers.append(w2n.word_to_num(numbers[i]))
-                        i += 1
-                elif (numbers[i] == '_'):
-                    if (numbers[i+1] in digits) and (p.number_to_words(finalNumbers[len(finalNumbers) - 1]) in digits):
-                        finalNumbers[len(finalNumbers) - 1] = int(str(finalNumbers[len(finalNumbers) - 1]) + '0' + str(w2n.word_to_num(numbers[i+1])))
-                    else:
-                        return []
-                    i += 2
-                elif (numbers[i] == '+'):
-                    if numbers[i+1] in multipliers:
-                        return []
-                    else:
-                        finalNumbers[len(finalNumbers) - 1] += w2n.word_to_num(numbers[i+1])
-                    i += 2
-            
-            for number in finalNumbers:
-                grandFinalNumbers.append(number)
-            numbers = []
-    
-    return grandFinalNumbers
 
 mathFile = open("arithmetic.txt", 'r')
 command = mathFile.read()
@@ -80,12 +24,15 @@ userText = command.split("##")[0]
 code = command.split("##")[1]
 numbers = []
 
-numbers = getNumbers(userText)
+for word in userText.split():
+    if(word.isdigit()):
+        numbers.append(int(word))
+        print(numbers)
 
 if(len(numbers) == 0):
     errorcall()
 
-if(code == 'addition'):
+if(code == 'add'):
     if(len(numbers) < 2):
         errorcall()
     else:
@@ -94,14 +41,14 @@ if(code == 'addition'):
             sum += number
         succcesscall(str(sum), code)
 
-elif(code == 'subtraction'):
+elif(code == 'sub'):
     if(len(numbers) != 2):
         errorcall()
     else:
         diff = numbers[0]-numbers[1]
         succcesscall(str(diff), code)
 
-elif(code == 'multiplication'):
+elif(code == 'mul'):
     if(len(numbers) < 2):
         errorcall()
     else:
@@ -110,7 +57,7 @@ elif(code == 'multiplication'):
             prod *= number
         succcesscall(str(prod), code)
 
-elif(code == 'division'):
+elif(code == 'div'):
     if(len(numbers) != 2):
         errorcall()
     else:
