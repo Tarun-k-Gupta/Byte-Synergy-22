db696ae2571fadf58daf6003b933c57d5066465c
Tarun Gupta <tarunkumar.gupta@iiitb.org>
2022-02-13 13:06:50 +0530

    added audio and links.txt

--- wik.py
+++ wik.py
@@ -7,6 +7,7 @@ import os
 
 with open('links.txt', 'r') as r:
     allLinks = r.readlines()    
+os.remove("links.txt")
     
 count = 0    
 wiklink=""
@@ -25,10 +26,7 @@ try:
     url = wiklink
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html5lib')
-    #printing the main heading
     print(soup.find('h1', {"id": "firstHeading"}).text)
-
-    #getting the first para below the main heading
     getp = soup.find('p',class_="")
     p_str = getp.text
     for i in range(1,51):
@@ -38,21 +36,10 @@ except:
     pass
 
 
-string = ""
-try:
-    text_file.write(p_str)
-    text_file = open("wiki.txt", "w")
-    text_file.close()
-    myText = ""
-    with open("wiki.txt", 'r') as s:
-        myText = s.read()
-    myLang = 'en-in'
-
-    myAudio = gTTS(text=myText, lang=myLang, slow=False)
-    os.remove("wiki.txt")
-    myAudio.save("wiki.wav")
-    text_file.close()
-    playsound('wiki.wav')
-    os.remove("wiki.wav")
-except:
-    pass
+def botSpeak(data):
+    with open("botSpeech.txt", 'w') as bS:
+        bS.write(data)
+    os.system("python3 botSpeak.py")
+    os.system("python3 playAudio.py")
+    
+botSpeak(p_str) 
