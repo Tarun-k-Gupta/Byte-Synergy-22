e0c56bc08ae3701f074b3ade30ce0756c68b0c8b
Tarun Gupta <tarunkumar.gupta@iiitb.org>
2022-02-13 12:30:19 +0530

    Update wik.py

--- wik.py
+++ wik.py
@@ -1,10 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://en.wikipedia.org/wiki/Web3"
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html5lib')
+from gtts import gTTS
+import sys
+from playsound import playsound
+import os
 
+with open('links.txt', 'r') as r:
+    allLinks = r.readlines()    
+    
+count = 0    
+wiklink=""
+for link in allLinks:
+    index = link.find("##TRUE")
+    if(index != -1):
+        count += 1
+        wiklink = link.replace("##TRUE", "")
+
+if(count == 0):
+    print(allLinks[0].replace("##FALSE",""))
+    
+
+p_str=""
 try:
+    url = wiklink
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
     #printing the main heading
     print(soup.find('h1', {"id": "firstHeading"}).text)
 
@@ -14,5 +34,25 @@ try:
     for i in range(1,51):
         p_str = p_str.replace("["+str(i)+"]","")
     print(p_str)
+except:
+    pass
+
+
+string = ""
+try:
+    text_file.write(p_str)
+    text_file = open("wiki.txt", "w")
+    text_file.close()
+    myText = ""
+    with open("wiki.txt", 'r') as s:
+        myText = s.read()
+    myLang = 'en-in'
+
+    myAudio = gTTS(text=myText, lang=myLang, slow=False)
+    os.remove("wiki.txt")
+    myAudio.save("wiki.wav")
+    text_file.close()
+    playsound('wiki.wav')
+    os.remove("wiki.wav")
 except:
     pass
