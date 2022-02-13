41cb0c6dec618755800fc1f654c2edc247ae7517
N V Sai Likhith <nvsailikhith@gmail.com>
2022-02-13 16:17:17 +0530

    Update searchEngine.py

--- searchEngine.py
+++ searchEngine.py
@@ -6,7 +6,12 @@ def searchFor():
     linkFile = open("links.txt", 'w')
     text = searchFile.read()
     searchList = search(text, tld='co.in', num=10, stop=10, pause=2)
+    wiki = "en.wikipedia.org"
     for link in searchList:
         linkFile.write(link + '\n')
+        if(link.split('/')[2] == wiki) :
+            linkFile.write("TRUE\n")
+        else:
+            linkFile.write("FALSE\n")
 
 searchFor()
