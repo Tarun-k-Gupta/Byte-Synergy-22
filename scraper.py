8e7e612b25b6a71765f797ea1b074e6662d8bc12
Siddharth-Yedlapati <siddharth.yedlapati@gmail.com>
2022-02-13 15:33:56 +0530

    fixed bugs, added audio

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
+import time
+from gtts import gTTS
 import sys
-
+from playsound import playsound
+import os
 import requests
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +14,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotInteractableException
 import lxml
 from bs4 import BeautifulSoup
+
 query = sys.argv[1]
 page ='https://www.worldometers.info/coronavirus/'
 options = Options()
@@ -27,4 +31,19 @@ total_recovered = str(list[2].text)
 active_cases = str(list[3].text)
 
 print("total_cases: " + total_cases + " total_deaths: " + total_deaths + " total_recovered: " + total_recovered + " active_cases: " + active_cases)
+output_string = "The total number of cases in " + query + " are " + total_cases + " the total number of deaths are " + total_deaths + " the total number of recovered cases are " + total_recovered + " and the number of active cases are " + active_cases
+text_file = open("coronavirus.txt", 'w')
+text_file.write(output_string)
+text_file.close()
+myText = ""
+with open("coronavirus.txt", 'r') as s:
+    myText = s.read()
+myLang = 'en-in'
+
+myAudio = gTTS(text=myText, lang=myLang, slow=False)
+os.remove("coronavirus.txt")
+myAudio.save("coronavirus.wav")
+text_file.close()
+playsound('coronavirus.wav')
+os.remove("coronavirus.wav")
 driver.quit()
